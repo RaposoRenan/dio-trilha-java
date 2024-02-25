5740d5f7f49156b00562a8b0b107f00d5ba5acee
Renan Raposo <renanraposo.eng@gmail.com>
2024-02-25 18:49:08 -0300

    Create desafio-dio-banco

---
+++ desafio-dio-banco/src/Main.java
@@ -0,0 +1,16 @@
+public class Main {
+    public static void main(String[] args) {
+        Cliente renan = new Cliente();
+        renan.setNome("Renan");
+
+        Conta cc = new ContaCorrente(renan);
+        Conta poupanca = new ContaPoupanca(renan);
+
+        cc.depositar(100);
+        cc.transferir(20, poupanca);
+
+        cc.imprimirExtrato();
+        poupanca.imprimirExtrato();
+    }
+
+}
