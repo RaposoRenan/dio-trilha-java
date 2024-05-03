e4bdcca4be984deddcef5fd054eabe0220c9680f
Renan Raposo <renanraposo.eng@gmail.com>
2024-05-03 17:13:42 -0300

    Commit Map

---
+++ Collections/src/main/java/map/OperacoesBasicas/AgendaContatos.java
@@ -0,0 +1,53 @@
+package main.java.map.OperacoesBasicas;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class AgendaContatos {
+    private Map<String, Integer> agendaContatoMap;
+
+    public AgendaContatos() {
+        this.agendaContatoMap = new HashMap<>();
+    }
+
+    public void adicionarContatos(String nome, Integer telefone) {
+        agendaContatoMap.put(nome, telefone);
+    }
+
+    public void removerContato(String nome) {
+        if (!agendaContatoMap.isEmpty()) {
+            agendaContatoMap.remove(nome);
+        }
+    }
+
+    public void exibirContatos() {
+        System.out.println(agendaContatoMap);
+    }
+
+    public Integer pesquisarPorNome(String nome) {
+        Integer numeroPorNome = null;
+        if (!agendaContatoMap.isEmpty()) {
+            numeroPorNome = agendaContatoMap.get(nome);
+        }
+        return numeroPorNome;
+    }
+
+    public static void main(String[] args) {
+        AgendaContatos agendaContatos = new AgendaContatos();
+
+        agendaContatos.adicionarContatos("Camila", 111111111);
+        agendaContatos.adicionarContatos("Camila", 222222222);
+        agendaContatos.adicionarContatos("Camila Cavalcante", 333333333);
+        agendaContatos.adicionarContatos("Camila DIO", 444444444);
+        agendaContatos.adicionarContatos("Maria Silva", 555555555);
+        agendaContatos.adicionarContatos("Camila", 666666666);
+
+        agendaContatos.exibirContatos();
+
+        agendaContatos.removerContato("Maria Silva");
+        agendaContatos.exibirContatos();
+
+        System.out.println("O número é: " + agendaContatos.pesquisarPorNome("Camila DIO"));
+
+    }
+}
