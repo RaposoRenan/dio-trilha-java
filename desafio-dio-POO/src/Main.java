d5ff85879709254afeaf1f2d29a427578c8d2d34
Renan Raposo <renanraposo.eng@gmail.com>
2024-06-09 22:10:20 -0300

    final

--- desafio-dio-POO/src/Main.java
+++ desafio-dio-POO/src/Main.java
@@ -1,4 +1,6 @@
+import br.com.dio.desafio.dominio.Bootcamp;
 import br.com.dio.desafio.dominio.Curso;
+import br.com.dio.desafio.dominio.Dev;
 import br.com.dio.desafio.dominio.Mentoria;
 
 import java.time.LocalDate;
@@ -20,8 +22,38 @@ public class Main {
         mentoria1.setDescricao("Mentoria Java");
         mentoria1.setData(LocalDate.now());
 
-        System.out.println(curso1);
+        /*System.out.println(curso1);
         System.out.println(curso2);
-        System.out.println(mentoria1);
+        System.out.println(mentoria1);*/
+
+        Bootcamp bootcamp = new Bootcamp();
+        bootcamp.setNome("Bootcamp Java Developer");
+        bootcamp.setDescricao("Descrição Bootcamp Java Developer");
+        bootcamp.getConteudos().add(curso1);
+        bootcamp.getConteudos().add(curso2);
+        bootcamp.getConteudos().add(mentoria1);
+
+        Dev devCamila = new Dev();
+        devCamila.setNome("Camila");
+        devCamila.inscreverBootcamp(bootcamp);
+        System.out.println("Conteúdos Inscritos " + devCamila.getNome() + ": " + devCamila.getConteudosInscritos());
+        devCamila.progredir();
+        System.out.println("-");
+        System.out.println("Conteúdos Inscritos " + devCamila.getNome() + ": " + devCamila.getConteudosInscritos());
+        System.out.println("Conteúdos Concluídos " + devCamila.getNome() + ": " + devCamila.getConteudosConcluidos());
+        System.out.println("XP: " + devCamila.calcularTotalXp());
+        System.out.println("-----------------");
+
+        Dev devRenan = new Dev();
+        devRenan.setNome("Renan");
+        devRenan.inscreverBootcamp(bootcamp);
+        System.out.println("Conteúdos Inscritos " + devRenan.getNome() + ": " + devRenan.getConteudosInscritos());
+        devRenan.progredir();
+        devRenan.progredir();
+        System.out.println("-");
+        System.out.println("Conteúdos Inscritos " + devRenan.getNome() + ": " + devRenan.getConteudosInscritos());
+        System.out.println("Conteúdos Concluídos " + devRenan.getNome() + ": " + devRenan.getConteudosConcluidos());
+        System.out.println("XP: " + devRenan.calcularTotalXp());
+        System.out.println("-----------------");
     }
 }
